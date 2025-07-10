83cf8cf7bcb6df42f1bcb778f4e393700b4a6319
Stephen Appah <appahstephen9@gmail.com>
2025-07-10 13:29:43 +0000

    Typescript error detection in development

---
+++ 01-why-typescript.ts
@@ -0,0 +1,9 @@
+
+const courseName = "Typescript Bootcamp";
+
+printCourseName(courseName);
+
+function printCourseName(name :string) {
+
+    console.log("The course name is " + name.toUpperCase());
+}
